66bdbf537a6e31388677a8454c28014fd27514ab
Rômulo Queiroz <rfcontatosvia@gmail.com>
2026-02-20 21:27:31 -0300

    feat: transforma lista de tarefas em quadro drag and drop

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CreateTaskComponent } from '../create-task/create-task.component';
 import { ConfirmationModalComponent } from '../confirmation-modal/confirmation-modal.component';
 import { UpdateTodoComponent } from '../update-todo/update-todo.component';
-import { ListTaskTodoService } from '../../Services/list-task-todo.service';
-import { MarkTaskAsUndoneService } from '../../Services/mark-task-done.service';
 import { ListTaskByUserService } from 'src/Services/list-task-by-user.service';
+import { MarkTaskAsUndoneService } from '../../Services/mark-task-done.service';
 import { userModel } from 'src/Models/user.model';
 
 @Component({
@@ -16,77 +13,84 @@ import { userModel } from 'src/Models/user.model';
   styleUrls: ['./task-list.component.css'],
 })
 export class TaskListComponent implements OnInit {
-  tasksToDo: any[] = [];
-  tarefasPorPagina = 6;
-  currentPage = 1;
-  totalPages = this.tasksToDo.length;
-  user : userModel = new userModel();
+  pendingTasks: any[] = [];
+  doneTasks: any[] = [];
+  user: userModel = new userModel();
+  draggingTaskId: number | null = null;
 
   constructor(
-    private http: HttpClient,
     private modalService: NgbModal,
-    private listTaskTodoService: ListTaskTodoService,
     private markTaskAsUndoneService: MarkTaskAsUndoneService,
-    private listTaskByUser: ListTaskByUserService,
+    private listTaskByUser: ListTaskByUserService
   ) {}
 
   ngOnInit() {
+    this.loadTasks();
+  }
+
+  loadTasks() {
     const user = localStorage.getItem('user');
+
     if (user) {
       this.user = JSON.parse(user);
     }
-    if (this.tasksToDo.length === 0 && this.user.id) {
-     this.listTaskByUser.listTaskByUserId(this.user.id).subscribe(
-        (response) => {
-          if (response && response.value && Array.isArray(response.value)) {
-            this.tasksToDo = response.value.filter((task) => !task.done);
-            console.log('Tarefas:', this.tasksToDo);
-          } else {
-            console.error('Resposta do servidor inválida:', response);
-          }
-        },
-        (error) => {
-          console.error('Erro ao listar as tarefas:', error);
-        }
-      );
+
+    if (!this.user.id) {
+      return;
     }
+
+    this.listTaskByUser.listTaskByUserId(this.user.id).subscribe(
+      (response) => {
+        if (response && response.value && Array.isArray(response.value)) {
+          this.pendingTasks = response.value.filter((task) => !task.done);
+          this.doneTasks = response.value.filter((task) => task.done);
+        } else {
+          console.error('Resposta do servidor inválida:', response);
+        }
+      },
+      (error) => {
+        console.error('Erro ao listar as tarefas:', error);
+      }
+    );
   }
-  
+
   openCreateTaskModal() {
-    const modalRef = this.modalService.open(CreateTaskComponent);
+    this.modalService.open(CreateTaskComponent).closed.subscribe(() => this.loadTasks());
   }
 
   openConfirmationModal(taskId: number) {
     const modalRef = this.modalService.open(ConfirmationModalComponent);
     modalRef.componentInstance.taskId = taskId;
+    modalRef.closed.subscribe(() => this.loadTasks());
   }
 
   openUpdateTaskModal(taskId: number) {
     const modalRef = this.modalService.open(UpdateTodoComponent);
     modalRef.componentInstance.taskId = taskId;
+    modalRef.closed.subscribe(() => this.loadTasks());
   }
 
-  markTaskAsUndone(taskId: number) {
+  markTaskAsDone(taskId: number) {
     this.markTaskAsUndoneService.markTaskAsDone(taskId).subscribe(
-      (response) => {
-        this.ngOnInit();
-        window.location.reload();
-      },
+      () => this.loadTasks(),
       (error) => {
         console.error('Erro ao marcar a tarefa como concluída:', error);
       }
     );
   }
 
-  previousPage() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
+  onDragStart(taskId: number) {
+    this.draggingTaskId = taskId;
+  }
+
+  onDropDone() {
+    if (this.draggingTaskId !== null) {
+      this.markTaskAsDone(this.draggingTaskId);
+      this.draggingTaskId = null;
     }
   }
 
-  nextPage() {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
-    }
+  clearDraggingTask() {
+    this.draggingTaskId = null;
   }
 }
